5c10c326976fdcab58acde43bb2ee5cdaff29b3b
jdelpego <jdelpego2@gmail.com>
2023-05-15 10:52:45 -0700

    new GYRO

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -18,6 +18,8 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.simulation.ADXRS450_GyroSim;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -25,13 +27,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Swerve extends SubsystemBase {
     public SwerveDriveOdometry swerveOdometry;
     public SwerveModule[] swerveMods;
-    public AHRS gyro;
+    public ADXRS450_Gyro gyro;
+    public ADXRS450_GyroSim gyroSim;
     private double m_simYaw;
 
 
     public Swerve() {
-        gyro = new AHRS(SPI.Port.kMXP);
-        SimDevice gyroSim = SimDevice.create("navX-Sensor[0]", SPI.Port.kMXP.value);
+        gyro = new ADXRS450_Gyro(SPI.Port.kMXP);
+        gyroSim = new ADXRS450_GyroSim(gyro);
         gyro.calibrate();
         zeroGyro();
 
@@ -108,11 +111,11 @@ public class Swerve extends SubsystemBase {
     }
 
     public void zeroGyro(){
-        gyro.zeroYaw();
+        gyro.reset();
     }
 
     public Rotation2d getYaw() {
-        return (Constants.Swerve.invertGyro) ? Rotation2d.fromDegrees(360 - gyro.getYaw()) : Rotation2d.fromDegrees(gyro.getYaw());
+        return (Constants.Swerve.invertGyro) ? Rotation2d.fromDegrees(360 - gyro.getAngle()) : Rotation2d.fromDegrees(gyro.getAngle());
     }
 
     public void resetModulesToAbsolute(){
@@ -136,11 +139,12 @@ public class Swerve extends SubsystemBase {
     public void simulationPeriodic() {
       ChassisSpeeds chassisSpeed = Constants.Swerve.swerveKinematics.toChassisSpeeds(getModuleStates());
       m_simYaw += chassisSpeed.omegaRadiansPerSecond * 0.02;
-  
       Unmanaged.feedEnable(20);
-      int dev = SimDeviceDataJNI.getSimDeviceHandle("navX-Sensor[0]");
-          SimDouble angle = new SimDouble(SimDeviceDataJNI.getSimValueHandle(dev, "Yaw"));
-          angle.set(-Units.radiansToDegrees(m_simYaw));
+      gyroSim.setAngle(-Units.radiansToDegrees(m_simYaw));
+          //Use below for NAVX2 Gyro. Check 5137 github prior to 5/15/2023 to find proper implementaTION
+            //angle.set(-Units.radiansToDegrees(m_simYaw));
+            //int dev = SimDeviceDataJNI.getSimDeviceHandle("navX-Sensor[0]");
+            // SimDouble angle = new SimDouble(SimDeviceDataJNI.getSimValueHandle(dev, "Yaw"));
     }
 
     
